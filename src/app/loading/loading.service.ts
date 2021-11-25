decd03f7ad2ce31b746afe7b9c8646a233536d44
JoseMMorales <Jose.Mula@met.ey.com>
2021-11-25 09:41:19 +0100

    Creating loading methods in service and observable in HTML for loading to be displayed

--- src/app/loading/loading.service.ts
+++ src/app/loading/loading.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 
 @Injectable()
 export class LoadingService {
 
+    loading$: Observable<boolean>;
 
-    constructor() {}
+    showLoaderUntilCompleted<T>(obs$: Observable<T>): Observable<T> {
+        return undefined;
+    }
+
+    loadingOn() {
+
+    }
+
+    loadingOff() {
+
+    }
 }
